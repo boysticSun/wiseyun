f4f8357a8dde3a0b7046ecc634dfa900e9cd5eae
vagrant <vagrant@homestead>
2022-08-22 08:36:16 +0000

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-start">
+      Copyright 2022-20xx xxxx.com. All Rights Reserved. xxxxx 版权所有
+    </p>
+
+    <p class="float-end">
+      资质认证
+    </p>
+  </div>
+</footer>
