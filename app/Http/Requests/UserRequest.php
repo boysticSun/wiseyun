e682286a8b99ada75c5e42f681394a6b7b5fdbfa
vagrant <vagrant@homestead>
2022-08-29 06:32:18 +0000

    限制头像分辨率

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -29,6 +29,7 @@ class UserRequest extends FormRequest
             'name' => 'required|between:3,25|unique:users,name,' . Auth::id(),
             // 'email' => 'required|email',
             'introduction' => 'max:80',
+            'avatar' => 'mimes:png,jpg,gif,jpeg|dimensions:min_width=208,min_height=208',
         ];
     }
 
@@ -39,6 +40,8 @@ class UserRequest extends FormRequest
             // 'name.regex' => '用户名只支持英文、数字、横杠和下划线。',
             'name.between' => '用户名必须介于 3 - 25 个字符之间。',
             'name.required' => '用户名不能为空。',
+            'avatar.mimes' =>'头像必须是 png, jpg, gif, jpeg 格式的图片',
+            'avatar.dimensions' => '图片的清晰度不够，宽和高需要 208px 以上',
         ];
     }
 }
