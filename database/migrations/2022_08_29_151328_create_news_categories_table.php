5f3f07d85b0862b4a3a52ff188dbbd8a6bf3f00d
vagrant <vagrant@homestead>
2022-08-29 07:24:55 +0000

    新闻分类模型和数据

---
+++ database/migrations/2022_08_29_151328_create_news_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->index()->comment('名称');
+            $table->text('description')->nullable()->comment('描述');
+            $table->integer('post_count')->default(0)->comment('新闻资讯数');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news_categories');
+    }
+};
---
+++ database/migrations/2022_08_29_151941_seed_news_categories_data.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        $newscategories = [
+            [
+                'name'        => '今日热点',
+                'description' => '今日热点新闻资讯',
+            ],
+            [
+                'name'        => '行业动态',
+                'description' => '行业动态新闻资讯',
+            ],
+            [
+                'name'        => '政策发布',
+                'description' => '政策发布新闻资讯',
+            ],
+            [
+                'name'        => '疫情资讯',
+                'description' => '疫情资讯信息',
+            ],
+        ];
+
+        DB::table('news_categories')->insert($newscategories);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        DB::table('categories')->truncate();
+    }
+};
